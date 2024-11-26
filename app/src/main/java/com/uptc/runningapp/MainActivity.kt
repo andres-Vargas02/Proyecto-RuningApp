677f85662591fd353c90711082b7d39808b46d3b
JulianRami <julian.ramirez04@uptc.edu.co>
2024-11-26 17:08:24 -0500

    refactor code

--- app/src/main/java/com/uptc/runningapp/MainActivity.kt
+++ app/src/main/java/com/uptc/runningapp/MainActivity.kt
@@ -3,11 +3,18 @@ package com.uptc.runningapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.uptc.runningapp.ui.navigation.AppNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MaterialTheme {
+                AppNavHost(races = listOf())
+            }
         }
     }
 }
+
+
--- app/src/main/java/com/uptc/runningapp/ui/composables/RaceItem.kt
+++ app/src/main/java/com/uptc/runningapp/ui/composables/RaceItem.kt
@@ -9,11 +9,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.uptc.runningapp.R
@@ -37,7 +37,7 @@ fun RaceItem(navController: NavController, race: Race) {
         ConstraintLayout(modifier = Modifier.padding(8.dp)) {
             val (icon, raceInfo) = createRefs()
             Image(
-                painter = painterResource(id = R.drawable.ic_race), // Asegúrate de que este recurso exista
+                painter = painterResource(id = R.drawable.ic_race),
                 contentDescription = "Race Icon",
                 modifier = Modifier
                     .size(48.dp)
