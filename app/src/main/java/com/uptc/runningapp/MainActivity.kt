b3653c26ec12808f451aac0d854d712b2e5f843b
soporteCamilo7 <soportemeganet7@gmail.com>
2024-11-26 16:04:12 -0500

    cambios 2

--- app/src/main/java/com/uptc/runningapp/MainActivity.kt
+++ app/src/main/java/com/uptc/runningapp/MainActivity.kt
@@ -3,9 +3,6 @@ package com.uptc.runningapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import com.uptc.runningapp.ui.navigation.AppNavHost
-import com.uptc.runningapp.ui.composables.generateSampleRaces
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
